eb4b986929dbf7a14ecd2ad39a57dc1edc4456f5
Alireza Kamali <kamalialireza788@gmail.com>
2023-10-22 14:27:07 +0330

    filter bug style

--- app/(routes)/category/[categoryId]/components/Filter.tsx
+++ app/(routes)/category/[categoryId]/components/Filter.tsx
@@ -53,8 +53,8 @@ const Filter: React.FC<FilterProps> = ({
                 {data.map((filter) => (
                     <div key={filter.id} className="flex items-center">
                         <Button
-                            className={`rounded-md text-sm text-gray-800 p-2 bg-white border border-gray-300
-                            ${selectedValue === filter.id && "bg-gray-900 text-zinc-100"}
+                            className={`rounded-md text-sm text-black p-2 bg-white border border-gray-300
+                            ${selectedValue === filter.id && "bg-black text-white"}
                             `}
                             onClick={() => onClick(filter.id)}
                         >
